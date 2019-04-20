5a7cffb60268c91ca0493fa10ebedbb9e0248346
David Graham <david.graham@strangebrewdesign.com>
2019-04-21 00:38:15 +0100

    Massive tidyup.

--- resources/views/entry/archive/default.php
+++ resources/views/entry/archive/default.php
@@ -1,6 +1,6 @@
 <article <?php Hybrid\Attr\display( 'entry' ) ?>>
 
-<?php Strangebrew\display_featured_image(); ?>
+	<?php Strangebrew\display_featured_image(); ?>
 
 	<header class="entry__header">
 		<?php Hybrid\Post\display_title() ?>
