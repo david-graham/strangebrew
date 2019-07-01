92c9232f3688bb17a16190cd06e5b7a5099fc16a
David Graham <david.graham@strangebrewdesign.com>
2019-07-01 21:44:30 +0100

    Add featured image support when not front page.

--- resources/views/entry/single/template-landing-page.php
+++ resources/views/entry/single/template-landing-page.php
@@ -2,7 +2,13 @@
 
 	<header class="entry__header alignfull">
 		<div class="overlay"></div>
-		<?php the_custom_header_markup() ?>
+		<?php if ( is_front_page() ) { 
+				the_custom_header_markup(); 
+			} else if ( has_post_thumbnail() ) { ?>
+			<div id="wp-custom-header" class="wp-custom-header">
+				<img src="<?php the_post_thumbnail_url( 'strangebrew-large' ); ?>">
+			</div>
+		<?php } ?>
 		<?php Strangebrew\display_content_before_more() ?>
 	</header>
 
